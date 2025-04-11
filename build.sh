62f66ac401b9765059d35e9ef0277ef4a51328b2
Lucas Aquino <lc.aquinodeoliveira@gmail.com>
2025-04-11 04:22:39 -0300

    refactor: created automated superusers

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+export DJANGO_SUPERUSER_USERNAME="clausalaerth"
+export DJANGO_SUPERUSER_EMAIL="lukhasaquino@gmail.com"
+export DJANGO_SUPERUSER_PASSWORD="4udacious_4_Aquin0"
+python manage.py createsuperuser --no-input
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
