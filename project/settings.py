57faa4b102f15d7ddd28364544e46b83167a552e
Lucas Aquino <lc.aquinodeoliveira@gmail.com>
2025-04-11 05:20:51 -0300

    fix: changed media root to fix broken images

--- project/settings.py
+++ project/settings.py
@@ -152,7 +152,9 @@ STATIC_ROOT = DATA_DIR / "static"
 
 MEDIA_URL = '/media/'
 # /data/web/media
-MEDIA_ROOT = DATA_DIR / "media"
+# MEDIA_ROOT = DATA_DIR / "media"
+
+MEDIA_ROOT = '/opt/render/project/src/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
