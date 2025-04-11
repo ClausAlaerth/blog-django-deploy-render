734a7bc680e5367746d77b89b8efd86149f41361
Lucas Aquino <lc.aquinodeoliveira@gmail.com>
2025-04-11 05:43:04 -0300

    refactor: reverted to local media

--- project/settings.py
+++ project/settings.py
@@ -152,9 +152,9 @@ STATIC_ROOT = DATA_DIR / "static"
 
 MEDIA_URL = '/media/'
 # /data/web/media
-# MEDIA_ROOT = DATA_DIR / "media"
+MEDIA_ROOT = DATA_DIR / "media"
 
-MEDIA_ROOT = '/opt/render/project/src/mediafiles/'
+# MEDIA_ROOT = '/opt/render/project/src/mediafiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
